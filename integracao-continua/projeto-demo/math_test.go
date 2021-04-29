d33ed0c9fb7937f58f67d2d8345053e2e6be340d
Gihovani Filipp <gihovani@gmail.com>
2021-04-29 11:17:43 -0300

    ci: adicionado configuracoes do sonnarqube para o projeto-demo

--- integracao-continua/projeto-demo/math_test.go
+++ integracao-continua/projeto-demo/math_test.go
@@ -2,10 +2,42 @@ package main
 
 import "testing"
 
-func TestSoma(t *testing.T) {
+func TestSum(t *testing.T) {
 	var result = 30
-	var total = Soma(15, 15)
+	var total = Sum(15, 15)
 	if total != result {
 		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado %d", total, result)
+	}
+}
+
+func TestSub(t *testing.T) {
+	var result = 0
+	var total = Sub(15, 15)
+	if total != result {
+		t.Errorf("Resultado da subtração é inválido: Resultado %d. Esperado %d", total, result)
+	}
+}
+
+func TestDiv(t *testing.T) {
+	var result = 2
+	var total = Div(6, 3)
+	if total != result {
+		t.Errorf("Resultado da divisão é inválido: Resultado %d. Esperado %d", total, result)
+	}
+}
+
+func TestTimes(t *testing.T) {
+	var result = 6
+	var total = Times(2, 3)
+	if total != result {
+		t.Errorf("Resultado da multiplicação é inválido: Resultado %d. Esperado %d", total, result)
+	}
+}
+
+func TestPow(t *testing.T) {
+	var result = 256
+	var total = Pow(2, 8)
+	if total != result {
+		t.Errorf("Resultado da Exponenciação é inválido: Resultado %d. Esperado %d", total, result)
 	}
 }
